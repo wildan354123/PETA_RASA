75dd41e50954607af57ef779c136fb7bb5af193a
mayangsarinr <mayangsarinr_2327240064@mhs.mdp.ac.id>
2024-11-30 09:51:09 +0700

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -14,5 +14,20 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    
-
+    return Scaffold(
+      // TODO : 1. Buat appbar dengan judul Peta Rasa
+      appBar: AppBar(title: Text('PETA RASA'),),
+      // TODO : 2. Buat body dengan GridView.Builder
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        padding: EdgeInsets.all(8),
+        itemCount: makananList.length,
+        itemBuilder: (context, index) {
+          final Makanan makanan = makananList[index];
+          return ItemCard(makanan: makanan);
+        },
+        // TODO : 3. Buat ItemCard sebagai return value dari GridView.Builder
+      ),
+    );
+  }
+}
