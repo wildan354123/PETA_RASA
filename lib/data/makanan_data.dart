64f83cec054e36d225dcab7a182c3b2395c45ca7
gledysapriliana <gledysaprill@gmail.com>
2024-12-15 14:20:17 +0700

    Update makanan_data.dart

--- lib/data/makanan_data.dart
+++ lib/data/makanan_data.dart
@@ -8,7 +8,8 @@ var makananList = [
       description: 'Pempek, mpek-mpek, atau dalam bahasa Indonesia gaul empek-empek, adalah makanan khas Palembang, Sumatera Selatan. Proses pembuatannya yakni dibuat dari daging ikan Tenggiri bisa juga ikan gabus yang digiling lembut, dicampur tepung kanji atau tepung sagu, serta dengan penambahan komposisi bahan lain seperti telur, bawang putih yang dihaluskan, penyedap rasa, dan garam. Pempek biasanya disajikan dengan kuah yang disebut cuko yang memiliki rasa asam, manis, dan pedas.',
       imageAsset: 'images/pempek.jpeg',
       imageAsset2: [
-        'images/resep_pempek.png',
+        'images/bahan_pempek.png',
+        'images/tutor_pempek.png',
       ],
       isFavorite: false,
   ),
@@ -19,7 +20,8 @@ var makananList = [
     description: 'Rendang (bahasa Minangkabau: randang; Jawi: رندڠ, Batak: ᯒᯉᯢᯝ) adalah hidangan berbahan dasar daging yang dihasilkan dari proses memasak suhu rendah dalam waktu lama dengan menggunakan aneka rempah-rempah dan santan. Proses memasaknya memakan waktu berjam-jam (biasanya sekitar empat jam) hingga yang tinggal hanyalah potongan daging berwarna hitam pekat dan dedak. Dalam suhu ruangan, rendang dapat bertahan hingga berminggu-minggu. Rendang yang dimasak dalam waktu yang lebih singkat dan santannya belum mengering disebut kalio, berwarna cokelat terang keemasan.',
     imageAsset: 'images/rendang.jpeg',
     imageAsset2: [
-      'images/resep_rendang.png',
+      'images/bahan_rendang.png',
+      'images/tutor_rendang.png',
     ],
     isFavorite: false,
   ),
@@ -30,7 +32,8 @@ var makananList = [
     description: 'Rendang (bahasa Minangkabau: randang; Jawi: رندڠ, Batak: ᯒᯉᯢᯝ) adalah hidangan berbahan dasar daging yang dihasilkan dari proses memasak suhu rendah dalam waktu lama dengan menggunakan aneka rempah-rempah dan santan. Proses memasaknya memakan waktu berjam-jam (biasanya sekitar empat jam) hingga yang tinggal hanyalah potongan daging berwarna hitam pekat dan dedak. Dalam suhu ruangan, rendang dapat bertahan hingga berminggu-minggu. Rendang yang dimasak dalam waktu yang lebih singkat dan santannya belum mengering disebut kalio, berwarna cokelat terang keemasan.',
     imageAsset: 'images/sate.jpeg',
     imageAsset2: [
-      'images/resep_sate_padang.png',
+      'images/bahan_sate_padang.png',
+      'images/tutor_sate_padang.png',
     ],
     isFavorite: false,
   ),
@@ -41,7 +44,8 @@ var makananList = [
     description: 'Nasi Goreng Kampung adalah hidangan tinggi kandungan protein yang mengkombinasikan nasi goreng kampung dengan gurih dari terasi, telor ceplok, acar, lalapan, dan kerupuk.',
     imageAsset: 'images/nasi_goreng_kampung.jpeg',
     imageAsset2: [
-      'images/resep_nasi_goreng_kampung.png',
+      'images/bahan_nasi_goreng_kampung.png',
+      'images/tutor_nasi_goreng_kampung.png',
     ],
     isFavorite: false,
   ),
@@ -52,7 +56,8 @@ var makananList = [
     description: 'Gudeg (bahasa Jawa: Guděg) adalah hidangan khas Daerah Istimewa Yogyakarta yang terbuat dari nangka muda yang dimasak dengan santan. Perlu waktu berjam-jam untuk membuat hidangan gudeg. Warna cokelat biasanya dihasilkan oleh daun jati yang dimasak bersamaan. Gudeg biasanya dimakan dengan nasi dan disajikan dengan kuah santan kental (areh), ayam kampung, telur, tempe, tahu, dan sambal goreng krecek.Gudeg sangat populer di Jawa, hidangan ini merupakan hidangan populer baik sebagai masakan rumahan maupun hidangan jalanan. Gudeg juga diproduksi secara industri sebagai makanan kaleng. Gudeg juga bisa ditemui di luar Indonesia, khususnya di negara tetangga seperti Malaysia dan Singapura.',
     imageAsset: 'images/gudeg.jpeg',
     imageAsset2: [
-     'images/resep_gudeg.png',
+     'images/bahan_gudeg.png',
+     'images/tutor_gudeg.png',
     ],
     isFavorite: false,
   ),
@@ -63,7 +68,8 @@ var makananList = [
     description: 'Bakso Malang adalah hidangan bakso yang banyak ditemui di seluruh belahan Kota Malang dan Kabupaten Malang, Jawa Timur. Mirip dengan hidangan bakso lainnya, hidangan tersebut terdiri dari daging yang dibentuk bundar dan mie. Bedanya ada pada penambahan kulit pangsit yang digoreng dan tahu yang digoreng.',
     imageAsset: 'images/bakso_malang.jpeg',
     imageAsset2: [
-      'images/resep_bakso_malang.png',
+      'images/bahan_bakso_malang.png',
+      'images/tutor_bakso_malang.png',
     ],
     isFavorite: false,
   ),
@@ -74,7 +80,8 @@ var makananList = [
         description: 'Rawon (Aksara Jawa:ꦫꦮꦺꦴꦤ꧀) adalah masakan khas Indonesia yang berasal dari Ponorogo, Jawa Timur, yang berupa sup daging berkuah hitam dengan campuran bumbu khas yang menggunakan kluwek.',
         imageAsset: 'images/rawon.jpeg',
         imageAsset2: [
-          'images/resep_rawon.png',
+          'images/bahan_rawon.png',
+          'images/tutor_rawon.png',
         ],
         isFavorite: false,
   ),
@@ -85,7 +92,8 @@ var makananList = [
         description: 'Soto Betawi merupakan jenis soto dari daerah DKI Jakarta. Selain jeroan, sering kali organ-organ lain juga disertakan, seperti mata, terpedo, penis sapi dan juga hati.',
         imageAsset: 'images/soto_betawi.jpeg',
         imageAsset2: [
-          'images/resep_soto_betawi.png',
+          'images/bahan_soto_betawi.png',
+          'images/tutor_soto_betawi.png',
         ],
         isFavorite: false,
   ),
@@ -96,7 +104,8 @@ var makananList = [
         description: 'Kerak telur adalah makanan asli daerah Jakarta (Betawi), dengan bahan-bahan beras ketan putih, telur ayam atau bebek, ebi (udang kering yang diasinkan), ditambah bawang merah goreng, lalu diberi bumbu yang dihaluskan berupa kelapa sangrai, cabai merah, kencur, jahe, kunyit,sereh halus, merica butiran, garam dan gula pasir.',
         imageAsset: 'images/kerak_telor.jpeg',
         imageAsset2: [
-          'images/resep_kerak_telur.png',
+          'images/bahan_kerak_telur.png',
+          'images/tutor_kerak_telur.png',
         ],
         isFavorite: false,
   ),
@@ -107,7 +116,8 @@ var makananList = [
         description: 'Papeda adalah makanan berupa bubur sagu yang berasal dari pesisir Papua. Hidangan ini biasanya disajikan dengan ikan tongkol atau bubara yang dibumbui dengan kunyit. Papeda berwarna putih dan bertekstur lengket menyerupai lem dengan rasa yang tawar.',
         imageAsset: 'images/papeda.jpeg',
         imageAsset2: [
-          'images/resep_papeda.png',
+          'images/bahan_papeda.png',
+          'images/tutor_papeda.png',
         ],
         isFavorite: false,
   ),
@@ -118,7 +128,8 @@ var makananList = [
         description: 'Klepon (bahasa Jawa: ꧋ꦏ꧀ꦊꦥꦺꦴꦤ꧀, translit. klêpon), dikenal pula sebagai onde-onde, adalah sejenis kue atau penganan kukus yang dibuat dari tepung ketan yang dibulatkan, diisi gula merah dan dibalurkan di atas kelapa parut hingga melekat.',
         imageAsset: 'images/klepon.jpeg',
         imageAsset2: [
-          'images/resep_klepon.png',
+          'images/bahan_klepon.png',
+          'images/tutor_klepon.png',
         ],
         isFavorite: false,
   ),
@@ -129,7 +140,8 @@ var makananList = [
         description: 'Gado-gado adalah makanan yang terdiri atas sayur-sayuran, kentang, tempe, tahu, telur rebus, dan lain-lain diberi bumbu sambal kacang dan sebagainya.',
         imageAsset: 'images/gado_gado.jpeg',
         imageAsset2: [
-          'images/resep_gado_gado.png',
+          'images/bahan_gado_gado.png',
+          'images/tutor_gado_gado.png',
         ],
         isFavorite: false,
   ),
@@ -140,7 +152,8 @@ var makananList = [
         description: 'Bika Ambon adalah kue tradisional khas Medan, Indonesia. Kue ini terbuat dari bahan-bahan seperti tepung tapioka, telur, gula, dan santan.',
         imageAsset: 'images/bika_ambon.jpeg',
         imageAsset2: [
-          'images/resep_bika_ambon.png',
+          'images/bahan_bika_ambon.png',
+          'images/tutor_bika_ambon.png',
         ],
         isFavorite: false,
   ),
@@ -151,7 +164,8 @@ var makananList = [
         description: 'Mi aceh adalah masakan mi pedas khas Aceh di Indonesia. Mi kuning tebal dengan irisan daging sapi, daging kambing atau makanan laut (udang dan cumi) disajikan dalam sup sejenis kari yang gurih dan pedas.',
         imageAsset: 'images/mie_aceh.jpeg',
          imageAsset2: [
-          'images/resep_mie_aceh.png',
+          'images/bahan_mie_aceh.png',
+          'images/tutor_mie_aceh.png',
         ],
         isFavorite: false,
   ),
@@ -162,7 +176,8 @@ var makananList = [
         description: 'Bubur ayam adalah salah satu jenis makanan dari Indonesia. Bubur nasi adalah beras yang dimasak dengan air yang banyak sehingga memiliki tekstur yang lembut dan berair. Bubur biasanya disajikan dalam suhu panas atau hangat.',
         imageAsset: 'images/bubur_ayam.jpeg',
          imageAsset2: [
-          'images/resep_bubur_ayam.png',
+          'images/bahan_bubur_ayam.png',
+          'images/tutor_bubur_ayam.png',
         ],
         isFavorite: false,
   ),
